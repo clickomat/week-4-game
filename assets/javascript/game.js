40ef259f363dc485249aab4c6dd89dfea0f42be8
clickomat <sconnell306@gmail.com>
2018-02-07 14:26:03 -0700

    numbers are showing up one at a time in the score html. Now I need to parse them and add them together while comparing the total each time to the target number for win loose functions.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,55 @@
 alert("hay!");
-
 var win = 0;
 var loose = 0;
 var target = Math.floor(Math.random() * 200);
 var score = 0;
 var blue = 0;
-var pink =  0;
-var yellow =  0;
-var purole =  0;
+var pink = 0;
+var yellow = 0;
+var purole = 0;
+var randomNum = [];
 var total = [];
 $(document).ready(function() {
-  $("#target").on("click", function() {
-
+  $("#go").on("click", function() {
     // Math.random an array the fill it with 4 crystals and give them those random values.
+    for (i = 0; i < 4; i++) {
+      randomNum[i] = Math.floor(Math.random() * 150);
+    }
+    blue = randomNum[0];
+    $("#blue").html(blue);
+    pink = randomNum[1];
+    yellow = randomNum[2];
+    purple = randomNum[3];
+    console.log(randomNum);
+    console.log(blue);
+    console.log(total);
+    // assign crystals with random numbers add the clicks form crystal together.
 
-    // add crystal clicks together
-
-// determine if the toatal and target values are the same for a win, incriment win, or over for a loss, incriment loss.
+    $(".crystal").on("click", function() {
+      // considered using parseint to return values to intergers and add them together on each click
+     $("#total").text(blue) 
+    
+    });
+    // determine if the toatal and target values are the same for a win, incriment win, or over for a loss, incriment loss.
     function targetreach() {
       for (var i = 0; i < total.length; i++) {
-        if (total[i] === targey) {
-          alert("You've do well young padowan! "+ total+ " and " +target+" are the same!");
+        if (total[i] === target) {
+          win++;
+          alert(
+            "You've do well young padowan! " +
+              total +
+              " and " +
+              target +
+              " are the same!"
+          );
+        } else if (total[i] > target) {
+          loose++;
+          alert(
+            "You have gone to far this time." + total + " is more than" + target
+          );
         }
-        win++
-        else if(total[i]>target){
-          alert("You have gone to far this time."+total+" is more than" + target);
-        }
-        loose++
       }
       return false;
     }
-    
-    
-    });
   });
 });
