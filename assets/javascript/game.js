7f988ed8218d9336a69a593b6a0f195b038f5fd5
clickomat <sconnell306@gmail.com>
2018-02-07 13:25:36 -0700

    starting pseudo code with some actual if/else statements. Trying to figure out how to populate an array with random numbers then assign thase to another array.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,37 @@
-$(document).ready(function() {
-  var win = 0;
-  var loose = 0;
-  var target = Math.floor(Math.random() * 200);
-  var score = 0;
+alert("hay!");
+
+var win = 0;
+var loose = 0;
+var target = Math.floor(Math.random() * 200);
+var score = 0;
 var blue = 0;
-var pink = 0;
-var yellow = 0;
-var purole= 0;
+var pink =  0;
+var yellow =  0;
+var purole =  0;
+var total = [];
+$(document).ready(function() {
+  $("#target").on("click", function() {
 
-  $("#target").text(target);
+    // Math.random an array the fill it with 4 crystals and give them those random values.
 
+    // add crystal clicks together
 
+// determine if the toatal and target values are the same for a win, incriment win, or over for a loss, incriment loss.
+    function targetreach() {
+      for (var i = 0; i < total.length; i++) {
+        if (total[i] === targey) {
+          alert("You've do well young padowan! "+ total+ " and " +target+" are the same!");
+        }
+        win++
+        else if(total[i]>target){
+          alert("You have gone to far this time."+total+" is more than" + target);
+        }
+        loose++
+      }
+      return false;
+    }
+    
+    
+    });
+  });
 });
