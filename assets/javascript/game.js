04e8a92f79034cb5e36345b6eae14ae67ea4b056
clickomat <sconnell306@gmail.com>
2018-02-07 08:52:30 -0700

    got the bones up and looking ok, added glow to crystals

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  var win = 0;
+  var loose = 0;
+  var target = Math.floor(Math.random() * 200);
+  var score = 0;
+var blue = 0;
+var pink = 0;
+var yellow = 0;
+var purole= 0;
+
+  $("#target").text(target);
+
+
+});
